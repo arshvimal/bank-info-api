e46c22ab53f3f6213570c4ddb1ae9ba6705b33b9
Arsh Vimal <arsh11vimal@gmail.com>
2024-06-25 14:55:09 +0530

    Endpoint Fix

--- bankapi/api/urls.py
+++ bankapi/api/urls.py
@@ -4,8 +4,8 @@ from .views import BankList, BranchList, BranchDetail, BankBranches, BankDetail
 
 urlpatterns = [
     path('bank/<pk>/', BankDetail.as_view()),
-    path('bank/list', BankList.as_view()),
+    path('bank/list/', BankList.as_view()),
     path('bank/<bank_id>/branches/', BankBranches.as_view()),
-    path('branch/list', BranchList.as_view()),
+    path('branch/list/', BranchList.as_view()),
     path('branch/<pk>/', BranchDetail.as_view()),
 ]
