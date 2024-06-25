ea075ffd58c59571f7d238f28a0d38f2afa00728
Arsh Vimal <arsh11vimal@gmail.com>
2024-06-25 13:24:38 +0530

    url path fixes

--- bankapi/api/urls.py
+++ bankapi/api/urls.py
@@ -3,9 +3,9 @@ from .views import BankList, BranchList, BranchDetail, BankBranches, BankDetail
 
 
 urlpatterns = [
-    path('banks/<pk>/', BankDetail.as_view()),
-    path('banks/list', BankList.as_view()),
-    path('banks/<bank_id>/branches/', BankBranches.as_view()),
-    path('branches/list', BranchList.as_view()),
-    path('branches/<pk>/', BranchDetail.as_view()),
+    path('bank/<pk>/', BankDetail.as_view()),
+    path('bank/list', BankList.as_view()),
+    path('bank/<bank_id>/branches/', BankBranches.as_view()),
+    path('branch/list', BranchList.as_view()),
+    path('branch/<pk>/', BranchDetail.as_view()),
 ]
